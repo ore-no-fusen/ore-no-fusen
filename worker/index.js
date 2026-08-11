c6a1f3bb177ed1df5194a6249507b06bdd2fa87b
uck48 <huchiki8@gmail.com>
2026-08-11 12:48:22 +0900

    chore: set PWA version to 5.1.4-pwa.1

--- worker/index.js
+++ worker/index.js
@@ -5,7 +5,7 @@
 import { resolvePushTitles } from './notification-title';
 import { closeClickedNotification, focusViewerOrOpenTarget } from './notification-click';
 
-const SW_VERSION = '5.0.0-pwa.7';
+const SW_VERSION = '5.1.4-pwa.1';
 
 self.addEventListener('install', () => {
   self.skipWaiting();
